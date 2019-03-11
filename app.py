b44f1bb57921359ad9a1d78d7e42fd076f955aca
Justin Pacquing <pacqqu@gmail.com>
2019-03-11 12:35:12 -0400

    parsing users and orgs

--- app.py
+++ app.py
@@ -1,25 +1,46 @@
-from neo4j import GraphDatabase
 import csv
-with open('eggs.csv', 'rb') as csvfile:
+from neo4j import GraphDatabase
 
 driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
 
-'''def add_person(driver, name):
-    with driver.session() as session:
-        # Caller for transactional unit of work
-        return session.write_transaction(create_person_node, name)'''
+#User Functions
+def clear_users(tx):
+    return tx.run("MATCH (n:User) DETACH DELETE n").data()
 
-def clear_db(tx):
-    return tx.run("MATCH (n:Person) DETACH DELETE n").data()
+def create_user_node(tx, userid, firstname, lastname):
+    return tx.run("CREATE (a:User {first_name: $firstname, last_name:$lastname, user_id:$userid}) RETURN id(a)", firstname=firstname, lastname=lastname, userid=userid).data()
 
-def create_person_node(tx, name):
-    return tx.run("CREATE (a:Person {name: $name}) RETURN id(a)", name=name).data()
+def print_users(tx):
+    return tx.run("MATCH (a:User) RETURN a").data()
 
-def print_persons(tx):
-    return tx.run("MATCH (a:Person) RETURN a").data()
+def parse_user_csv():
+    with open('Data/user.csv', 'rb') as usercsv:
+        userreader = csv.reader(usercsv, delimiter=',', quotechar='|')
+        for row in userreader:
+            if row[0] != 'User_id':
+                print(row)
+            print(session.write_transaction(create_user_node,row[0], row[1], row[2]))
 
+#Org Functions
+def clear_orgs(tx):
+    return tx.run("MATCH (n:Organization) DETACH DELETE n").data()
+
+def create_org_node(tx, orgid, firstname, lastname):
+    return tx.run("CREATE (a:Organization {name: $firstname, last_name:$lastname, org_id:$orgid}) RETURN id(a)", firstname=firstname, lastname=lastname, orgid=orgid).data()
+
+def print_orgs(tx):
+    return tx.run("MATCH (a:Organization) RETURN a").data()
+
+def parse_org_csv():
+    with open('Data/org.csv', 'rb') as orgcsv:
+        orgreader = csv.reader(orgcsv, delimiter=',', quotechar='|')
+        for row in orgreader:
+            if row[0] != 'Organization_id':
+                print(row)
+            print(session.write_transaction(create_org_node,row[0], row[1], row[2]))
 
 with driver.session() as session:
-    print session.write_transaction(clear_db)
-    print session.write_transaction(create_person_node,'Noam')
-    print session.read_transaction(print_persons)
+    print(session.write_transaction(clear_orgs))
+    parse_user_csv()
+    #print session.write_transaction(create_user_node,'Noam')
+    print(session.read_transaction(print_users))
