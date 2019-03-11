837a75429d239fdfc700f0805e19c85eea29564a
Noam Sohn <noam.sohn63@myhunter.cuny.edu>
2019-03-11 12:46:14 -0400

    set up init for mongo

--- init_mongo.py
+++ init_mongo.py
@@ -1,47 +1,90 @@
+# NOTE TO SELF:
+#  * skip first row of each file
+
 import csv
 import pprint
 from pymongo import MongoClient
 client = MongoClient()
+# client.drop_database('<DBNAME>')
 
 db = client.test_database
+# client.db.command("dropDatabase")
 
+
+# userreader = csv.reader(usercsv, delimiter=',', quotechar='|')
+#         for row in userreader:
+#                 print row
 def initUser():
     users = db.users
 
-    user_loc = ("Data/user.xlsx")
-    wb = xlrd.open_workbook(user_loc)
-    sheet = wb.sheet_by_index(0)
-
-    # Read csv and extract data
-    for row in range(1,sheet.nrows):
-        arr = sheet.row_values(row)
-        user = {"user_id": arr[0],
-                "first_name": arr[1],
-                "last_name": arr[2]}
-        users_id = users.insert_one(user).inserted_id
-    # TEST
-    # pprint.pprint(users.find_one({"first_name":"Justin"}))
+    with open('Data/user.csv') as usercsv:
+        user_reader = csv.reader(usercsv, delimiter=',', quotechar='|')
+        for row in user_reader:
+            user = {"user_id": row[0],
+                    "first_name": row[1],
+                    "last_name": row[2]}
+            # print(user)
+            users_id = users.insert_one(user).inserted_id
+            # TEST
+            pprint.pprint(users.find_one({"first_name":"Justin"}))
 
 
 def initSkill():
     skills = db.skills
-
-    skill_loc = ("Data/skill.xlsx")
-    wb = xlrd.open_workbook(skill_loc)
-    sheet = wb.sheet_by_index(0)
-    sheet.cell_value(1, 0)
-
-    for row in range(1, sheet.nrows):
-        arr = sheet.row_values(row)
-        skill = {"user_id":arr[0],
-                "skill": arr[1],
-                "skill_level": arr[2]
-                }
-        skill_id = skills.insert_one(skill).inserted_id
-    # TEST
-    # pprint.pprint(skills.find_one({"user_id":1}))
+    with open('Data/skill.csv') as skillcsv:
+        skill_reader = csv.reader(skillcsv, delimiter=',', quotechar='|')
+        for row in skill_reader:
+            skill = {"user_id":row[0],
+                    "skill": row[1],
+                    "skill_level": row[2]
+                    }
+            skill_id = skills.insert_one(skill).inserted_id
+            # TEST
+            # pprint.pprint(skills.find_one({"user_id":1}))
 
 def initProject():
     projects = db.projects
+    with open('Data/project.csv') as projectcsv:
+        project_reader = csv.reader(projectcsv, delimiter=',', quotechar='|')
+        for row in project_reader:
+            project = {"user_id":row[0],
+                    "project": row[1],
+                    }
+            project_id = projects.insert_one(project).inserted_id
+             # TEST
+            # pprint.pprint(projects.find_one({"user_id":1}))
 
-    project_loc = ("Data/project.xlsx")
+def initOrganization():
+    organizations = db.organizations
+    with open('Data/organization.csv') as organizationcsv:
+        organization_reader = csv.reader(organizationcsv, delimiter=',', quotechar='|')
+        for row in organization_reader:
+            organization = {"user_id":row[0],
+                            "organization":row[1],
+                            "organization_type": row[2]}
+            organization_id = organizations.insert_one(organization).inserted_id
+            # TEST
+           # pprint.pprint(projects.find_one({"user_id":1}))
+
+def initInterest():
+    interests = db.interests
+    with open('Data/interest.csv') as interestcsv:
+        interest_reader = csv.reader(interestcsv, delimiter=',', quotechar='|')
+        for row in interest_reader:
+            interest = {"user_id":row[0],
+                        "interest":row[1],
+                        "interest_level": row[2]}
+            interest_id = interests.insert_one(interest).inserted_id
+            # TEST
+            pprint.pprint(interests.find_one({"user_id":'1'}))
+def initDistance():
+    distances = db.distances
+    with open('Data/distance.csv') as distancecsv:
+        distance_reader = csv.reader(distancecsv, delimiter=',', quotechar='|')
+        for row in distance_reader:
+            distance = {"organization_1":row[0],
+                        "organization_2":row[1],
+                        "distance": row[2]}
+            distance_id = distances.insert_one(distance).inserted_id
+            # TEST
+            # pprint.pprint(distances.find_one({"organization_1":'Hunter'}))
