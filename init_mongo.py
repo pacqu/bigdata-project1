8df9bcaa5bfb8f430eae8be111b8a4583e00a71c
Noam Sohn <noam.sohn63@myhunter.cuny.edu>
2019-03-13 13:55:00 -0400

    readline to get rid of first row

--- init_mongo.py
+++ init_mongo.py
@@ -18,6 +18,7 @@ def initUser():
     users = db.users
 
     with open('Data/user.csv') as usercsv:
+        usercsv.readline()
         user_reader = csv.reader(usercsv, delimiter=',', quotechar='|')
         for row in user_reader:
             user = {"user_id": row[0],
@@ -26,12 +27,13 @@ def initUser():
             # print(user)
             users_id = users.insert_one(user).inserted_id
             # TEST
-            pprint.pprint(users.find_one({"first_name":"Justin"}))
+            # pprint.pprint(users.find_one({"first_name":"Justin"}))
 
 
 def initSkill():
     skills = db.skills
     with open('Data/skill.csv') as skillcsv:
+        skillcsv.readline()
         skill_reader = csv.reader(skillcsv, delimiter=',', quotechar='|')
         for row in skill_reader:
             skill = {"user_id":row[0],
@@ -45,6 +47,7 @@ def initSkill():
 def initProject():
     projects = db.projects
     with open('Data/project.csv') as projectcsv:
+        projectcsv.readline()
         project_reader = csv.reader(projectcsv, delimiter=',', quotechar='|')
         for row in project_reader:
             project = {"user_id":row[0],
@@ -57,6 +60,7 @@ def initProject():
 def initOrganization():
     organizations = db.organizations
     with open('Data/organization.csv') as organizationcsv:
+        organizationcsv.readline()
         organization_reader = csv.reader(organizationcsv, delimiter=',', quotechar='|')
         for row in organization_reader:
             organization = {"user_id":row[0],
@@ -69,6 +73,7 @@ def initOrganization():
 def initInterest():
     interests = db.interests
     with open('Data/interest.csv') as interestcsv:
+        interestcsv.readline()
         interest_reader = csv.reader(interestcsv, delimiter=',', quotechar='|')
         for row in interest_reader:
             interest = {"user_id":row[0],
@@ -76,10 +81,11 @@ def initInterest():
                         "interest_level": row[2]}
             interest_id = interests.insert_one(interest).inserted_id
             # TEST
-            pprint.pprint(interests.find_one({"user_id":'1'}))
+            # pprint.pprint(interests.find_one({"user_id":'1'}))
 def initDistance():
     distances = db.distances
     with open('Data/distance.csv') as distancecsv:
+        distancecsv.readline()
         distance_reader = csv.reader(distancecsv, delimiter=',', quotechar='|')
         for row in distance_reader:
             distance = {"organization_1":row[0],
