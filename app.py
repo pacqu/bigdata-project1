626bb17ee3a8f8568fdb6902d5ef9a061bc39b49
Noam Sohn <noam.sohn63@myhunter.cuny.edu>
2019-03-18 14:31:35 -0400

    join func

--- app.py
+++ app.py
@@ -37,7 +37,8 @@ def get_trusted_col_of_col(origin_id, interests):
     for trust in mongo_results['trusted']:
         trust['common_trusted'] = ", ".join(neo_results['common_trust'][trust['user_id']])
     return mongo_results
-
+def organize_list(input_list):
+    return ", ".join(["{k} - {v}".format(k=k,v=str(v)) for d in input_list if d != {} for (k,v) in d.items()])
 def find_nearby_users():
     origin_id = int(input("Please input an origin id: "))
     command_results = get_uni_connected_users(origin_id)
@@ -47,8 +48,8 @@ def find_nearby_users():
         user = command_results[i]
         if i == 0:
             print('Origin User:')
-            skills = ", ".join(["{k} - {v}".format(k=k,v=str(v)) for d in user['skills'] if d != {} for (k,v) in d.items()])
-            interests = ", ".join(["{k} - {v}".format(k=k,v=str(v)) for d in user['interests'] if d != {} for (k,v) in d.items()])
+            skills = organize_list(user['skills'])
+            interests = organize_list(user['interests'])
             origin_org = user['org']['name']
             print('{fname} {lname} - Organization: {orgname}({orgtype})'.format(fname=user['first_name'], lname=user['last_name'], orgname=user['org']['name'],orgtype=user['org']['org_type']))
             print('\tSkills: {skills}'.format(skills=skills))
@@ -63,9 +64,8 @@ def find_nearby_users():
                     print('Given User is not a University User!')
         else:
             print('{fname} {lname} - Organization: {orgname}({orgtype}) - Distance from {o_org}: {dist}'.format(fname=user['first_name'], lname=user['last_name'], orgname=user['org']['name'], orgtype=user['org']['org_type'], o_org=origin_org, dist=user['total_dist']))
-            skills = ", ".join(["{k} - {v}".format(k=k,v=str(v)) for d in user['skills'] if d != {} for (k,v) in d.items()])
-            interests = ", ".join(["{k} - {v}".format(k=k,v=str(v)) for d in user['interests'] if d != {} for (k,v) in d.items()])
-            #print()
+            skills = organize_list(user['skills'])
+            interests = organize_list(user['interests'])
             if len(skills):
                 print('\tCommon Skills: {skills} (Common Skill Score: {sscore})'.format(skills=skills, sscore=user['totalSkillMatch']))
             if len(interests):
@@ -81,8 +81,8 @@ def find_user():
     if not user:
         print('User does not exist')
         return
-    skills = ", ".join(["{k} - {v}".format(k=k,v=str(v)) for d in user['skills'] if d != {} for (k,v) in d.items()])
-    interests = ", ".join(["{k} - {v}".format(k=k,v=str(v)) for d in user['interests'] if d != {} for (k,v) in d.items()])
+    skills = organize_list(user['skills'])
+    interests = organize_list(user['interests'])
     print('{fname} {lname}'.format(fname=user['first_name'], lname=user['last_name']))
     print('\tSkills: {skills}'.format(skills=skills))
     print('\tInterests: {interests}'.format(interests=interests))
